e8008f7860c0fd7c23915c3c34ba061a7ab0f06c
foehammer127 <foehammer@disroot.org>
2024-04-30 14:02:23 -0500

    feat(services/vaultwarden): add log file

--- services/vaultwarden/default.nix
+++ services/vaultwarden/default.nix
@@ -16,7 +16,6 @@ in
         What external port to serve over.
       '';
     };
-
   };
 
   config = mkIf cfg.enable {
@@ -37,6 +36,7 @@ in
         DOMAIN = "https://passwords.foehammer.me";
         ROCKET_LOG = "critical";
         SIGNUPS_ALLOWED = false;
+        LOG_FILE = "/var/log/bitwarden.log";
       };
 
       environmentFile = config.age.secrets.vaultwardenenv.path;
