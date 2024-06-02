e7185780444f3a1cbf095ab64a07074a6e9af9e2
foehammer127 <foehammer@disroot.org>
2024-06-02 12:35:13 -0500

    fix(keys.nix): add foe-pythagoras to keys.nix

--- keys.nix
+++ keys.nix
@@ -5,4 +5,6 @@
 
   foe-feynman = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINjs2NaYUvSdixDixupfzTAX1qfCipymWyCR7sG9S64l foe@feynman";
   foe-leni-windows = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOBvkK6GBNA9peMFhopsE82vB3AQH9v5V+f7oEZzaCkQ spa\25lorenzorg@14S-21-070";
+
+  foe-pythagoras = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBxhsmHrbVR9oHVjyMQs2giJXmonbHAu1m8Qy3PE3QjP foehammer@pythagoras";
 }
