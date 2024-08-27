29880296ef6c3ad1ec9bb7bb1c0511d6f6346996
foehammer127 <foehammer@disroot.org>
2024-08-27 10:24:24 -0500

    Switch to stable nixpkgs.
    Switch to stable nixpkgs, to avoid constant system breakage.

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
 
     flake-parts.url = "github:hercules-ci/flake-parts";
 
@@ -20,7 +20,7 @@
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
