604ad9aded4d136f2a938d46258cf995d49c0cc7
foehammer127 <foehammer@disroot.org>
2024-11-07 21:00:34 -0600

    Add tab movements to nvim.

--- users/foehammer/nvim/config/lua/FoehammerVim/vim.lua
+++ users/foehammer/nvim/config/lua/FoehammerVim/vim.lua
@@ -46,6 +46,17 @@ local function set_vim_keymaps()
 	vim.keymap.set('n', '<right>', '<Nop>')
 	vim.keymap.set('n', '<up>', '<Nop>')
 	vim.keymap.set('n', '<down>', '<No')
+
+	-- Tabs:
+	vim.api.nvim_set_keymap("n", "<leader>ta", ":$tabnew<CR>", { noremap = true })
+	vim.api.nvim_set_keymap("n", "<leader>tc", ":tabclose<CR>", { noremap = true })
+	vim.api.nvim_set_keymap("n", "<leader>to", ":tabonly<CR>", { noremap = true })
+	vim.api.nvim_set_keymap("n", "<leader>tn", ":tabn<CR>", { noremap = true })
+	vim.api.nvim_set_keymap("n", "<leader>tp", ":tabp<CR>", { noremap = true })
+	-- move current tab to previous position
+	vim.api.nvim_set_keymap("n", "<leader>tmp", ":-tabmove<CR>", { noremap = true })
+	-- move current tab to next position
+	vim.api.nvim_set_keymap("n", "<leader>tmn", ":+tabmove<CR>", { noremap = true })
 end
 
 
